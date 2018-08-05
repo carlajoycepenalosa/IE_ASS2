1c17f89b31cd2939fae28f747b9190e5355a629b
carlajoycepenalosa <42107247+carlajoycepenalosa@users.noreply.github.com>
2018-08-05 14:29:09 +0800

    Add files via upload

---
+++ PEÑALOSA.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    cout << ">>>>>>>>>>WELCOME TO CJ CAFE<<<<<<<<<< \n \n";
+    cout << "-------------CJ CAFE MENU------------- \n \n";
+    cout << "ESPRESSO \n";
+
+    int Espresso = 65;
+    int Mocha = 75;
+    int Capuccino = 80;
+    int Caramel = 70;
+    int Americano = 60;
+
+    cout << endl << "Espresso   .......... " << Espresso;
+    cout << endl << "Mocha      .......... " << Mocha;
+    cout << endl << "Capuccino  .......... " << Capuccino;
+    cout << endl << "Caramel    .......... " << Caramel;
+    cout << endl << "Americano  .......... " << Americano;
+
+    cout << endl << endl << "DESSERTS";
+
+    int Cheesecake = 105;
+    int Salad = 120;
+    int Macaroon = 115;
+    int Muffin = 110;
+    int Tiramisu = 150;
+
+    cout << endl << endl << "Cheesecake .......... " << Cheesecake;
+    cout << endl << "Salad      .......... " << Salad;
+    cout << endl << "Macaroon   .......... " << Macaroon;
+    cout << endl << "Muffin     .......... " << Muffin;
+    cout << endl << "Tiramisu   .......... " << Tiramisu;
+    
+    cout << endl << endl << "SNACKS";
+
+    int Fries = 99;
+    int Smores = 99;
+    int Churros = 99;
+    int Quesadilla = 199;
+    int Nachos = 249;
+
+    cout << endl << endl << "Fries      .......... " << Fries;
+    cout << endl << "Smores     .......... " << Smores;
+    cout << endl << "Churros    .......... " << Churros;
+    cout << endl << "Quesadilla .......... " << Quesadilla;
+    cout << endl << "Nachos     .......... " << Nachos;
+
+    return 0;
+}
